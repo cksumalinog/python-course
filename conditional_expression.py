c250df11c6e3737b7efcb7a0c16b96335f30a3e9
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-01-17 17:31:26 +0800

    Learned conditional_expression

---
+++ conditional_expression.py
@@ -0,0 +1,18 @@
+num = 5
+a = 6
+b = 7
+age = 13
+temperature = 20
+user_role = "student"
+
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+print(access_level)
+#weather = "It is HOT" if temperature > 20 else "It is COLD"
+#print(weather)
+#status = "Adult" if age >= 18 else "Child"
+#print(status)
+#print("Positive" if num > 0 else "Negative")
+#result = "EVEN" if num % 2 == 0 else "ODD"
+#print(min_num)
+#max_num = a if a > b else b
+#min_num = a if a < b else b
