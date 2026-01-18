d1b50718e6a82b66d5586ea8b02843aabd9e46a7
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-01-18 12:14:44 +0800

    Learned few string methods

---
+++ string_methods.py
@@ -0,0 +1,16 @@
+print(help(str))
+
+#name = input("Enter your full name: ")
+#phone_number = input("Enter your phone number: ")
+
+#result = len(name)
+#result = name.find(" ")
+#result = name.rfind("q")
+#name = name.capitalize()
+#name = name.upper()
+#name = name.lower()
+#result = name.isdigit()
+#result = name.isalpha()
+#result = phone_number.count("-")
+#phone_number = phone_number.replace("-", "")
+#print(phone_number)
