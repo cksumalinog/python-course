ad19282691e53ed485d375dc63c912efdd3961f5
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-01-23 21:51:11 +0800

    Learned for_loops

---
+++ for_loops.py
@@ -0,0 +1,9 @@
+credit_card = "1234-5678-9012"
+for counter in credit_card:
+    print(counter)
+print()
+for x in range(1, 21):
+    if x == 13:
+        break # or continue
+    else:
+        print(x)
