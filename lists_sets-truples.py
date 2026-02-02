cc75c3679572335e0a9fc2d5cdefc7e7a56d2789
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-02-02 20:40:30 +0800

    Learned about lists_sets_truples

--- lists_sets-truples.py
+++ lists_sets-truples.py
@@ -1,6 +1,23 @@
-fruits = ["apple", "orange", "banana", "coconut"]
-print(dir(fruits))
-print(fruits[:3])
-for fruit in fruits:
-    print(fruit)
-#will start tomo hehe
+#fruits = ["apple", "orange", "banana", "coconut"]
+#fruits = {"apple", "orange", "banana", "coconut"}
+#fruits = ("apple", "orange", "banana", "coconut")
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("rambutan" in fruits)
+
+#fruits[0] = "Rambutan"
+#fruits.append("pineapple")
+#fruits.remove("orange")
+#fruits.insert(0, "pineapple")
+#fruits.sort()
+#fruits.reverse()
+#fruits.clear()
+#print(fruits.index("apple"))
+#print(fruits.count("pineapple"))
+
+#print(fruits)
+#print(fruits[:3])
+#for fruit in fruits:
+#    print(fruit)
+
