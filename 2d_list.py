72d52a02182f7799c215e86d90de56ba0ce6c9c7
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-02-05 21:22:15 +0800

    Created a simple numpad in console using 2D array list

--- 2d_list.py
+++ 2d_list.py
@@ -1,8 +1,17 @@
 groceries = [["apple", "orange", "banana"],
-             ["potato", "cabbage", "kangkong"],
-             ["pork", "chicken", "beef"]]
+            ["potato", "cabbage", "kangkong"],
+            ["pork", "chicken", "beef"]]
 
 for grocery in groceries:
     for collection in grocery:
         print(collection, end=" ")
+    print()
+
+num_pad = ((1, 2, 3),
+            (4, 5, 6),
+            (7, 8 ,9),
+            ("*", 0, "#"))
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
     print()
